5d7faf5138f0e75ec4c0483529f4bc0a59a42feb
Saurabh <saurabkumar679@gmail..com>
2022-03-30 12:09:11 +0530

    Add logger functionality

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export class Log{
+    static success(message: string){
+       console.log(`%c ${message}`, 'color: green');
+    }
+
+    static error(message: string){
+        console.log(`%c ${message}`, 'color: red');
+    }
+
+    static info(message: string){
+        console.log(`%c ${message}`, 'color: black', 'background-color: yellow');
+    }
+}
